86bf2c7de7059949c1758150dbf7bfc3a526759f
weibo <bo.wei@healthbok.com>
2017-03-13 18:22:18 +0800

    做了一些初始化操作

--- slidingswitch/src/main/java/com/example/slidingswitch/SlidingButton.java
+++ slidingswitch/src/main/java/com/example/slidingswitch/SlidingButton.java
@@ -1,5 +1,6 @@
 package com.example.slidingswitch;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
@@ -15,6 +16,11 @@ public class SlidingButton extends View {
 
     //创建画笔
     private Paint paint;
+    //长宽
+    private int width,height;
+    //属性动画
+    private ValueAnimator valueAnimator;
+
 
     public SlidingButton(Context context) {
         super(context);
@@ -22,6 +28,7 @@ public class SlidingButton extends View {
 
     public SlidingButton(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public SlidingButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -30,16 +37,27 @@ public class SlidingButton extends View {
 
     private void init() {
         //初始化一些参数
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);   //画笔抗锯齿
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+
+        valueAnimator = new ValueAnimator();
 
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        width= w;
+        height = h;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
+        canvas.drawLine(0,height/2,width,height/2,paint);
+
     }
 }
